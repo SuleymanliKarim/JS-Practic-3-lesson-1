9520157dc220aac79c51d1a90b9efa2a1fa4d347
unknown <ksuleymanli99@gmail.com>
2023-08-01 16:49:56 +0300

    JS Practic lesson 1

---
+++ assets/JS/style.js
@@ -0,0 +1,56 @@
+{
+    let a=5
+    let b=7
+    console.log(a+b)
+}
+
+{
+    let a=8
+    let b=9
+    console.log(a%b)
+}
+
+{
+    let a=8
+    let b=9
+    console.log(b%a)
+}
+
+{
+    let a=5
+    let b=2
+    console.log(a%b)
+}
+{
+    let a=5
+    let b=2
+    console.log(b%a)
+}
+    
+{
+    const a=999
+    const b="0"
+    console.log(typeof a)
+}
+{
+    const a=999
+    const b="0"
+    console.log(typeof b)
+}
+
+{
+    const a= " on iki"
+    const b=12
+    console.log(a*b)
+}
+// {
+//     const a=on2
+//     const b=12
+//     console.log(a*b)
+// }
+
+{
+    let a=3
+    let b=8
+    a===6? console.log(a): console.log(b)
+}
